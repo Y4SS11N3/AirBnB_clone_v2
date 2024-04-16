3f4219ad4128eb84be5c480e7180a55ae77711c3
YASSINE <contact@yassine.fun>
2024-04-15 17:11:05 -0700

    Update State class for SQLAlchemy integration and relationship with City

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+import models
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """
+        Returns the list of City instances with
+        state_id equal to the current State.id
+        """
+        city_list = models.storage.all(City)
+        return [city for city in city_list if city.state_id == self.id]
