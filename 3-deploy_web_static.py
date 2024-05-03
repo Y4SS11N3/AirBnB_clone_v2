16ab14e4fcfe8f1b1472ba046c5cd0edb3ae5868
YASSINE <contact@yassine.fun>
2024-05-03 11:38:49 -0700

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,15 +12,13 @@ env.hosts = ['54.90.33.112', '23.23.75.134']
 @runs_once
 def do_pack():
     """Archive the static files"""
-    try:
-        datetime_obj = datetime.now().strftime("%Y%m%d%H%M%S")
-        file_path = "versions/web_static_{}.tgz".format(datetime_obj)
-        if os.path.isdir("versions") is False:
-            local("mkdir versions")
-        local("tar -cvzf {} web_static".format(file_path))
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    file_path = 'versions/web_static_' + now + '.tgz'
+    local('mkdir -p versions')
+    result = local('tar -cvzf {} web_static'.format(file_path))
+    if result.succeeded:
         return file_path
-    except Exception as e:
-        print(f"Error: {e}")
+    else:
         return None
 
 
@@ -31,27 +29,18 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    try:
-        put(archive_path, "/tmp/")
-        file_name = os.path.basename(archive_path)
-        folder_path = file_name.strip(".tgz")
-        run("mkdir -p /data/web_static/releases/{}/".format(folder_path))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            file_name, folder_path))
-        run("rm /tmp/{}".format(file_name))
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(
-                folder_path, folder_path))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(
-            folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current"
-            .format(folder_path))
-        print("New version deployed!")
-        return True
-    except Exception as e:
-        print(f"Error: {e}")
-        return False
+    file_name = archive_path.split('/')[-1]
+    folder_name = file_name.replace('.tgz', '')
+    put(archive_path, '/tmp/')
+    run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file_name, folder_name))
+    run('rm /tmp/{}'.format(file_name))
+    run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(folder_name, folder_name))
+    run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
+    run('rm -rf /data/web_static/current')
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_name))
+    print("New version deployed!")
+    return True
 
 
 def deploy():
@@ -59,6 +48,6 @@ def deploy():
     Full deployment function
     """
     archive_path = do_pack()
-    if archive_path is None:
+    if not archive_path:
         return False
     return do_deploy(archive_path)
