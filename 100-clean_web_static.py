e46607e205af3927556ae8ae16f981faa91070f1
YASSINE <contact@yassine.fun>
2024-05-03 10:38:51 -0700

    Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,18 +8,14 @@ from fabric.api import *
 
 env.hosts = ['54.90.33.112', '23.23.75.134']
 
-
 def do_clean(number=0):
     """Deletes out-of-date archives"""
     number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    # Clean local archives
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        local("ls -1t web_static_*.tgz | tail -n +{} | xargs -r rm -f".format(number + 1))
 
+    # Clean remote archives
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        run("ls -1t | grep 'web_static_' | tail -n +{} | xargs -r rm -rf".format(number + 1))
