d651591bbc6cc90d2238b2faa647d8d472d88422
YASSINE <contact@yassine.fun>
2024-05-17 09:28:00 -0700

    Implement states and state routes to display state information and their cities

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,14 +8,16 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/states', strict_slashes=False)
 def states():
     """
     Display a HTML page with a list of all State objects
     present in DBStorage sorted by name.
     """
-    states = sorted(storage.all(State).values(), key=lambda x: x.name)
-    return render_template('9-states.html', states=states)
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
 
 @app.route('/states/<id>', strict_slashes=False)
 def state(id):
@@ -27,12 +29,14 @@ def state(id):
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """
     Remove the current SQLAlchemy session after each request.
     """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
