27efe5653613f99743478b059465826b01af3bbb
YASSINE <contact@yassine.fun>
2024-05-18 03:21:37 -0700

    Implement states_list route to display a list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-""" Flask web application """
-
+"""
+A script that starts a Flask web application.
+"""
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a HTML page with list states and cities """
-    states = storage.all("State").values()
+    """
+    Display a HTML page with a list of all State objects
+    present in DBStorage sorted by name.
+    """
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """ Remove the current SQLAlchemy Session """
+    """
+    Remove the current SQLAlchemy session after each request.
+    """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
