8b58be9b03420122694f6c2378d78ce8bfea412c
YASSINE <contact@yassine.fun>
2024-05-18 04:20:28 -0700

    Improve engines: Add close() method to FileStorage and DBStorage, implement cities getter in State

--- models/state.py
+++ models/state.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from os import getenv
+from models.city import City
+import models
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='delete', backref='state')
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """cities getter in case of using file storage"""
-            from models.city import City
-            from models import storage
-            cities_list = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """Return the list of City objects
+        from storage linked to the current State."""
+        from models import storage
+        from models.city import City
+
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
