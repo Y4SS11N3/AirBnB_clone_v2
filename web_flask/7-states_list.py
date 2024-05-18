5e20e5bda5f0fd3bd519bc87dde6391292fa63bb
YASSINE <contact@yassine.fun>
2024-05-18 04:18:48 -0700

    Improve engines: Add close() method to FileStorage and DBStorage, implement cities getter in State

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+
+The app liste on 0.0.0.0, port 5000.
+
 """
-A script that starts a Flask web application.
-"""
-from flask import Flask, render_template
 from models import storage
+from flask import Flask
+from flask import render_template
 from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
+@app.route("/states_list", strict_slashes=False)
+def lis_states():
+    """Displa an HTML page  is lis of al State obj in DBS.
+
+    States are sort by name.
     """
-    Display a HTML page with a list of all State objects
-    present in DBStorage sorted by name.
-    """
-    states = sorted(storage.all(State).values(), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    Remove the current SQLAlchemy session after each request.
-    """
+def zerdown(exc):
+    """Remove the current SQLA session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
