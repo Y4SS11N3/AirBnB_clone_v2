8e62d90f1822a0d2d9f16857e5330d0c2f2ef55f
YASSINE <contact@yassine.fun>
2024-05-20 00:16:17 -0700

    Implement states list functionality, fix issues, and improve code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
+
+"""Starts a Flask web application.
+
+The app listens on 0.0.0.0, port 5000.
 """
-A script that starts a Flask web application.
-"""
-from flask import Flask, render_template
+
 from models import storage
+from flask import Flask
+from flask import render_template
 from models.state import State
 
 app = Flask(__name__)
 
+@app.route("/states_list", strict_slashes=False)
+def list_states():
+    """Display an HTML page with a list of all State objects in DBStorage.
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
+    States are sorted by name.
     """
-    Display a HTML page with a list of all State objects
-    present in DBStorage sorted by name.
-    """
-    states = sorted(storage.all(State).values(), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template("7-states_list.html", states=states)
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    Remove the current SQLAlchemy session after each request.
-    """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
